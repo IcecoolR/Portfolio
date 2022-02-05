1ab1451c36fdaf09903684a456c710e807ecb032
IcecoolR <45355900+IcecoolR@users.noreply.github.com>
2022-02-05 12:39:26 +0000

    Styled Social Media Links
    Added some styles to the social media links and tweaked the JS. Decided to remove the global constant variables as it made it too messy.

--- scripts.js
+++ scripts.js
@@ -6,45 +6,29 @@ const projectsButton = document.getElementById("projectsButton");
 const interestsButton = document.getElementById("interestsButton");
 
 const aboutButtonHandler = () => {
-  const divAbout = document.getElementById("about");
-  const divWorkEducation = document.getElementById("workEducation");
-  const divProjects = document.getElementById("projects");
-  const divInterests = document.getElementById("interests");
-  divAbout.style.display = "block";
-  divWorkEducation.style.display = "none";
-  divProjects.style.display = "none";
-  divInterests.style.display = "none";
+  document.getElementById("about").style.display = "block";
+  document.getElementById("workEducation").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("interests").style.display = "none";
 }
 
 const wordEducationButtonHandler = () => {
-  const divAbout = document.getElementById("about");
-  const divWorkEducation = document.getElementById("workEducation");
-  const divProjects = document.getElementById("projects");
-  const divInterests = document.getElementById("interests");
-  divAbout.style.display = "none";
-  divWorkEducation.style.display = "block";
-  divProjects.style.display = "none";
-  divInterests.style.display = "none";
+  document.getElementById("about").style.display = "none";
+  document.getElementById("workEducation").style.display = "block";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("interests").style.display = "none";
 }
 
 const projectsButtonHandler = () => {
-  const divAbout = document.getElementById("about");
-  const divWorkEducation = document.getElementById("workEducation");
-  const divProjects = document.getElementById("projects");
-  const divInterests = document.getElementById("interests");
-  divAbout.style.display = "none";
-  divWorkEducation.style.display = "none";
-  divProjects.style.display = "block";
-  divInterests.style.display = "none";
+  document.getElementById("about").style.display = "none";
+  document.getElementById("workEducation").style.display = "none";
+  document.getElementById("projects").style.display = "block";
+  document.getElementById("interests").style.display = "none";
 }
 
 const interestsButtonHandler = () => {
-  const divAbout = document.getElementById("about");
-  const divWorkEducation = document.getElementById("workEducation");
-  const divProjects = document.getElementById("projects");
-  const divInterests = document.getElementById("interests");
-  divAbout.style.display = "none";
-  divWorkEducation.style.display = "none";
-  divProjects.style.display = "none";
-  divInterests.style.display = "block";
+  document.getElementById("about").style.display = "none";
+  document.getElementById("workEducation").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("interests").style.display = "block";
 }
