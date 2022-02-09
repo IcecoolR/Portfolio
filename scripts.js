74fd4cfe08b6de293e346c87970fe29a2f616691
IcecoolR <45355900+IcecoolR@users.noreply.github.com>
2022-02-09 14:42:43 +0000

    Added Focus Accessibility
    Users can now use the keyboard to interact with the website.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const downArrow = document.getElementById("downArrow");
+
 const aboutdiv = document.getElementById("about");
 const workEducationdiv = document.getElementById("workEducation");
 const projectsdiv = document.getElementById("projects");
@@ -10,54 +12,79 @@ const workEducationbtn = document.getElementById("workEducationButton");
 const projectsbtn = document.getElementById("projectsButton");
 const interestsbtn = document.getElementById("interestsButton");
 
-const scrollToMain = () => {
-  document.getElementById("main").scrollIntoView();
+const scrollToMain = (e) => {
+  if (e.type == "click" || e.code == "Enter" || e.code == "NumpadEnter" ) {
+    document.getElementById("main").scrollIntoView();
+  }
 }
 
-const aboutButtonHandler = () => {
-  aboutdiv.style.display = "block";
-  workEducationdiv.style.display = "none";
-  projectsdiv.style.display = "none";
-  interestsdiv.style.display = "none";
+const aboutButtonHandler = (e) => {
+  if (e.type == "click" || e.code == "Enter" || e.code == "NumpadEnter" ) {
+    aboutdiv.style.display = "block";
+    workEducationdiv.style.display = "none";
+    projectsdiv.style.display = "none";
+    interestsdiv.style.display = "none";
 
-  aboutbtn.classList.add("selectedButton");
-  workEducationbtn.classList.remove("selectedButton");
-  projectsbtn.classList.remove("selectedButton");
-  interestsbtn.classList.remove("selectedButton");
+    aboutbtn.classList.add("selectedButton");
+    workEducationbtn.classList.remove("selectedButton");
+    projectsbtn.classList.remove("selectedButton");
+    interestsbtn.classList.remove("selectedButton");
+  }
 }
 
-const wordEducationButtonHandler = () => {
-  aboutdiv.style.display = "none";
-  workEducationdiv.style.display = "block";
-  projectsdiv.style.display = "none";
-  interestsdiv.style.display = "none";
+const wordEducationButtonHandler = (e) => {
+  if (e.type == "click" || e.code == "Enter" || e.code == "NumpadEnter" ) {
+    aboutdiv.style.display = "none";
+    workEducationdiv.style.display = "block";
+    projectsdiv.style.display = "none";
+    interestsdiv.style.display = "none";
 
-  aboutbtn.classList.remove("selectedButton");
-  workEducationbtn.classList.add("selectedButton");
-  projectsbtn.classList.remove("selectedButton");
-  interestsbtn.classList.remove("selectedButton");
+    aboutbtn.classList.remove("selectedButton");
+    workEducationbtn.classList.add("selectedButton");
+    projectsbtn.classList.remove("selectedButton");
+    interestsbtn.classList.remove("selectedButton");
+  }
 }
 
-const projectsButtonHandler = () => {
-  aboutdiv.style.display = "none";
-  workEducationdiv.style.display = "none";
-  projectsdiv.style.display = "block";
-  interestsdiv.style.display = "none";
+const projectsButtonHandler = (e) => {
+  if (e.type == "click" || e.code == "Enter" || e.code == "NumpadEnter" ) {
+    aboutdiv.style.display = "none";
+    workEducationdiv.style.display = "none";
+    projectsdiv.style.display = "block";
+    interestsdiv.style.display = "none";
 
-  aboutbtn.classList.remove("selectedButton");
-  workEducationbtn.classList.remove("selectedButton");
-  projectsbtn.classList.add("selectedButton");
-  interestsbtn.classList.remove("selectedButton");
+    aboutbtn.classList.remove("selectedButton");
+    workEducationbtn.classList.remove("selectedButton");
+    projectsbtn.classList.add("selectedButton");
+    interestsbtn.classList.remove("selectedButton");
+  }
 }
 
-const interestsButtonHandler = () => {
-  aboutdiv.style.display = "none";
-  workEducationdiv.style.display = "none";
-  projectsdiv.style.display = "none";
-  interestsdiv.style.display = "block";
+const interestsButtonHandler = (e) => {
+  if (e.type == "click" || e.code == "Enter" || e.code == "NumpadEnter" ) {
+    aboutdiv.style.display = "none";
+    workEducationdiv.style.display = "none";
+    projectsdiv.style.display = "none";
+    interestsdiv.style.display = "block";
 
-  aboutbtn.classList.remove("selectedButton");
-  workEducationbtn.classList.remove("selectedButton");
-  projectsbtn.classList.remove("selectedButton");
-  interestsbtn.classList.add("selectedButton");
+    aboutbtn.classList.remove("selectedButton");
+    workEducationbtn.classList.remove("selectedButton");
+    projectsbtn.classList.remove("selectedButton");
+    interestsbtn.classList.add("selectedButton");
+  }
 }
+
+downArrow.addEventListener("click", scrollToMain);
+downArrow.addEventListener("keydown", scrollToMain);
+
+aboutbtn.addEventListener("click", aboutButtonHandler);
+aboutbtn.addEventListener("keydown", aboutButtonHandler);
+
+workEducationbtn.addEventListener("click", wordEducationButtonHandler);
+workEducationbtn.addEventListener("keydown", wordEducationButtonHandler);
+
+projectsbtn.addEventListener("click", projectsButtonHandler);
+projectsbtn.addEventListener("keydown", projectsButtonHandler);
+
+interestsbtn.addEventListener("click", interestsButtonHandler);
+interestsbtn.addEventListener("keydown", interestsButtonHandler);
