606399d68b44ce1dce7f03884265c7d8a51f1049
Richard <IcecoolRi@gmail.com>
2022-10-20 09:05:24 +0100

    Added Spotify API Code
    Added code to query the Spotify API, however the access token expires after about 30 minutes.

--- scripts.js
+++ scripts.js
@@ -15,14 +15,14 @@ const interestsbtn = document.getElementById("interestsButton");
 const chessBlitzRatingHighest = document.getElementById("chessBlitzRatingHighest");
 
 const scrollToMain = (e) => {
-  if (e.type == "click" || e.code == "Enter" || e.code == "NumpadEnter" ) {
+  if (e.type == "click" || e.code == "Enter" || e.code == "NumpadEnter") {
     document.getElementById("main").scrollIntoView();
     document.activeElement.blur();
   }
 }
 
 const aboutButtonHandler = (e) => {
-  if (e.type == "click" || e.code == "Enter" || e.code == "NumpadEnter" ) {
+  if (e.type == "click" || e.code == "Enter" || e.code == "NumpadEnter") {
     aboutdiv.style.display = "block";
     workEducationdiv.style.display = "none";
     projectsdiv.style.display = "none";
@@ -36,7 +36,7 @@ const aboutButtonHandler = (e) => {
 }
 
 const wordEducationButtonHandler = (e) => {
-  if (e.type == "click" || e.code == "Enter" || e.code == "NumpadEnter" ) {
+  if (e.type == "click" || e.code == "Enter" || e.code == "NumpadEnter") {
     aboutdiv.style.display = "none";
     workEducationdiv.style.display = "block";
     projectsdiv.style.display = "none";
@@ -50,7 +50,7 @@ const wordEducationButtonHandler = (e) => {
 }
 
 const projectsButtonHandler = (e) => {
-  if (e.type == "click" || e.code == "Enter" || e.code == "NumpadEnter" ) {
+  if (e.type == "click" || e.code == "Enter" || e.code == "NumpadEnter") {
     aboutdiv.style.display = "none";
     workEducationdiv.style.display = "none";
     projectsdiv.style.display = "block";
@@ -64,7 +64,7 @@ const projectsButtonHandler = (e) => {
 }
 
 const interestsButtonHandler = (e) => {
-  if (e.type == "click" || e.code == "Enter" || e.code == "NumpadEnter" ) {
+  if (e.type == "click" || e.code == "Enter" || e.code == "NumpadEnter") {
     aboutdiv.style.display = "none";
     workEducationdiv.style.display = "none";
     projectsdiv.style.display = "none";
@@ -74,26 +74,59 @@ const interestsButtonHandler = (e) => {
     workEducationbtn.classList.remove("selectedButton");
     projectsbtn.classList.remove("selectedButton");
     interestsbtn.classList.add("selectedButton");
+
+    getInterestData();
   }
 }
 
-fetch("https://api.chess.com/pub/player/icecoolr/stats")
-  .then( (response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      console.log("Failed to get chess.com stats: " + response);
+function getInterestData() {
+
+  fetch("https://api.chess.com/pub/player/icecoolr/stats")
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.log("Failed to get chess.com stats: " + response);
+        chessBlitzRatingHighest.textContent = "about 1000";
+      }
+    })
+    .then((data) => {
+      chessBlitzRatingHighest.textContent = data.chess_blitz.best.rating;
+    })
+    .catch((error) => {
+      console.log("Error with chess.com stats.");
       chessBlitzRatingHighest.textContent = "about 1000";
+    });
+
+
+  let userAccessToken = "BQCebsfCyfEB8x4N8jmQBz1qrd9fnjgjkr1B668HWsAxCVyfn_n26SSh2VW87CiQwJRANesh99t9_Z02YKPL_EO0HYFfTy4UVTAy8anXpJx1JFHd1S8Ezhqsacw80U3xlPXHxbe2RkbTPJbEhQQVhBCgeO9IEuaXZmDWH27sZ7P02xEyqsCZ";
+  fetch("https://api.spotify.com/v1/me/player/currently-playing", {
+    method: "GET",
+    headers: {
+      'Authorization': `Bearer ${userAccessToken}`,
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
     }
   })
-  .then( (data) => {
-    chessBlitzRatingHighest.textContent = data.chess_blitz.best.rating;
-  })
-  .catch( (error) => {
-    console.log("Error with chess.com stats.");
-    chessBlitzRatingHighest.textContent = "about 1000";
-  });
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.log("Failed to get Spotify stats: " + response);
+      }
+    })
+    .then((data) => {
+      // TO DO Populate elements with data! (Last Spotifiy song or currently playing)
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log("Error with Spotify.");
+    });
 
+}
+
+
+getInterestData();
 
 downArrow.addEventListener("click", scrollToMain);
 downArrow.addEventListener("keydown", scrollToMain);
